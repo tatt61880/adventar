197bf7b3e85b036affe2f5884f634b8c705465dc
tatt61880 <tatt61880@gmail.com>
2018-10-28 05:38:38 +0900

    .

--- 2016/1213/src/isPrimeDrill.js
+++ 2016/1213/src/isPrimeDrill.js
@@ -1,87 +1,91 @@
 function isPrime(num){
-	if(num % 2 == 0 && num != 2) return false;
-	for(var i = 3; i * i <= num; i += 2){
-		if(num % i == 0) return false;
-	}
-	return true;
+  if(num % 2 == 0 && num != 2) return false;
+  for(var i = 3; i * i <= num; i += 2){
+    if(num % i == 0) return false;
+  }
+  return true;
 }
 // ↑素数かどうかを判定する関数はたったの7行です。ね、簡単でしょう？
 
-function PrimeFactorizationNotation(num){
-	var ret = "";
-	if(num % 2 == 0){
-		var count = 0;
-		while(num % 2 == 0){
-			num /= 2;
-			count++;
-		}
-		ret += 2;
-		if(count != 1){
-			ret += " ^ " + count;
-		}
-	}
-	for(var i = 3; i <= num; i += 2){
-		if(num % i == 0){
-			var count = 0;
-			while(num % i == 0){
-				num /= i;
-				count++;
-			}
-			if(ret != ""){
-				ret += " x ";
-			}
-			if(count == 1){
-				ret += i;
-			}else{
-				ret += i + " ^ " + count;
-			}
-			i = 3;
-		}
-	}
-	return ret;
+function primeFactorizationNotation(num){
+  var ret = "";
+  if(num % 2 == 0){
+    var count = 0;
+    while(num % 2 == 0){
+      num /= 2;
+      count++;
+    }
+    ret += 2;
+    if(count != 1){
+      ret += " ^ " + count;
+    }
+  }
+  for(var i = 3; i <= num; i += 2){
+    if(num % i == 0){
+      var count = 0;
+      while(num % i == 0){
+        num /= i;
+        count++;
+      }
+      if(ret != ""){
+        ret += " x ";
+      }
+      if(count == 1){
+        ret += i;
+      }else{
+        ret += i + " ^ " + count;
+      }
+      i = 3;
+    }
+  }
+  return ret;
 }
 
 var targetNum; // 判定対象の数
-var count_total = 0; // 回答数
+var countTotal = 0; // 回答数
 var count_corret = 0; // 正答数
 function onButtonClick_Judge(event, isYes){
-	event.preventDefault();
-	var result = ((isYes == isPrime(targetNum)) ? '○' : '×') + ': ';
-	count_corret += (isYes == isPrime(targetNum)) ? 1 : 0;
-	count_total++;
-	if(isPrime(targetNum)){
-		result += targetNum + " is a prime number.\n";
-	}else{
-		result += targetNum + " = " + PrimeFactorizationNotation(targetNum) + "\n";
-	}
-	UpdateNum();
-	UpdateResult(result);
+  event.preventDefault();
+
+  var isCorrect = isYes == isPrime(targetNum);
+  var result = (isCorrect ? '○' : '×') + ': ';
+  count_corret += isCorrect ? 1 : 0;
+  countTotal++;
+  if(isCorrect == isYes){
+    result += targetNum + " is a prime number.\n";
+  }else{
+    result += targetNum + " = " + primeFactorizationNotation(targetNum) + "\n";
+  }
+  updateNum();
+  updateResult(result);
 }
 
-var objTextarea_Results;
-var objSpan_Results;
-function UpdateResult(result){
-	objTextarea_Results.value = result + objTextarea_Results.value;
-	objSpan_Results.innerHTML = "正答数/回答数 = " + count_corret + "/" + count_total + " = " + (Math.round((100 * count_corret / count_total) * 100) / 100) + "%";
+var spanTargetNum;
+var textareaResults;
+var spanResults;
+function updateResult(result){
+  textareaResults.value = result + textareaResults.value;
+  spanResults.innerHTML = "正答数/回答数 = " + count_corret + "/" + countTotal + " = " + (Math.round((100 * count_corret / countTotal) * 100) / 100) + "%";
 }
 
 var last = [1, 3, 7, 9, 11, 13];
-function UpdateNum(){
-	var rand1 = Math.floor(Math.random() * 13) + 1;
-	var rand2 = Math.floor(Math.random() * 14);
-	var rand3 = last[Math.floor(Math.random() * 6)];
-	targetNum = Number(String(rand1) + String(rand2) + String(rand3));
-	UpdateText();
+function updateNum(){
+  var rand1 = Math.floor(Math.random() * 13) + 1;
+  var rand2 = Math.floor(Math.random() * 14);
+  var rand3 = last[Math.floor(Math.random() * 6)];
+  targetNum = Number(String(rand1) + String(rand2) + String(rand3));
+  updateText();
 }
 
-function UpdateText(){
-	document.getElementById('TargetNum').innerHTML = "「" + targetNum + "」は、<br>";
+function updateText(){
+  spanTargetNum.innerHTML = "「" + targetNum + "」は、<br>";
 }
 
 window.addEventListener('load', function(){
-	targetNum = 1213;
-	objTextarea_Results = document.getElementById("Textarea_Results");
-	objTextarea_Results.value = "";
-	objSpan_Results = document.getElementById("Span_Results");
-	UpdateText();
+  spanTargetNum = document.getElementById('spanTargetNum');
+  textareaResults = document.getElementById("textareaResults");
+  textareaResults.value = "";
+  spanResults = document.getElementById("spanResults");
+  targetNum = 1213;
+  updateText();
 });
--- adventar.php
+++ adventar.php
@@ -1,7 +1,7 @@
 <?php
 function ShowErrorMessage($message)
 {
-	print <<< EOD
+  print <<< EOD
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,11 +20,15 @@ EOD;
 }
 
 // URLを解析します。
+$current_dir = "adventar";
+$base_name = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . "/" . $current_dir;
+$url = (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+
 $REQUEST_URI = $_SERVER["REQUEST_URI"];  // 例:/adventar/2016/kuin/1/
-preg_match("/^\/adventar\/(\d+)\/([^\/]+)\/(\d+)\/((?:preview)?)$/", $REQUEST_URI, $match);
+preg_match("/^\/$current_dir\/(\d+)\/([^\/]+)\/(\d+)\/((?:preview)?)$/", $REQUEST_URI, $match);
 if(count($match) != 5){
-	ShowErrorMessage("不正なURLです。");
-	return;
+  ShowErrorMessage("不正なURLです。");
+  return;
 }
 $year = $match[1];
 $dir = $match[2];
@@ -49,177 +53,197 @@ $utc = time();
 
 if(0){
 }else if($day <= 0 || 25 < $day){
-	ShowErrorMessage("無効なURLです。");
+  ShowErrorMessage("無効なURLです。");
 }else if(!file_exists($theme_dat)){
-	ShowErrorMessage("テーマが準備できていません。");
+  ShowErrorMessage("テーマが準備できていません。");
 }else if(!file_exists($fileurl)){
-	ShowErrorMessage("記事が準備できていません。");
+  ShowErrorMessage("記事が準備できていません。");
 }else{
-	$theme = trim(file_get_contents($theme_dat));
-	$adventar_id = 0;
-	if(file_exists($adventarId_dat)){
-		$adventar_id = trim(file_get_contents($adventarId_dat));
-	}else{
-		$adventar_id = -1;
-	}
+  $theme = trim(file_get_contents($theme_dat));
+  $adventar_id = 0;
+  if(file_exists($adventarId_dat)){
+    $adventar_id = trim(file_get_contents($adventarId_dat));
+  }else{
+    $adventar_id = -1;
+  }
+  $title = "$theme Advent Calendar ${year} - ${day}日目";
 
-	$header = "";
-	$footer = "";
-	$main_html = "";
-	$is_include_kuin_code = false;
-	{
-		$prev_exist = file_exists($fileurl_prev);
-		$post_exist = file_exists($fileurl_post);
-		$disp_div = ($prev_exist || $post_exist) ? "display" : "none";
-		$prev_visivility = $prev_exist ? "visible" : "hidden";
-		$post_visivility = $post_exist ? "visible" : "hidden";
-		$day_m1 = $day - 1;
-		$day_p1 = $day + 1;
+  $header = "";
+  $footer = "";
+  $main_html = "";
+  $is_include_kuin_code = false;
+  {
+    $prev_exist = file_exists($fileurl_prev);
+    $post_exist = file_exists($fileurl_post);
+    $disp_div = ($prev_exist || $post_exist) ? "inline" : "none";
+    $prev_visivility = $prev_exist ? "visible" : "hidden";
+    $post_visivility = $post_exist ? "visible" : "hidden";
+    $day_m1 = $day - 1;
+    $day_p1 = $day + 1;
 
-		$header = <<< EOD
+    $header = <<< EOD
 <div style="display:$disp_div">
-	<a href="../$day_m1/$additional" style="visibility:$prev_visivility">←前の日の記事</a>　　
-	<a href="../$day_p1/$additional" style="visibility:$post_visivility">→次の日の記事</a>
+  <a href="../$day_m1/$additional" style="visibility:$prev_visivility">←前の日の記事</a>　　
+  <a href="../$day_p1/$additional" style="visibility:$post_visivility">→次の日の記事</a>
 </div>
 <hr>
 EOD;
 
-		$footer = <<< EOD
+    $footer = <<< EOD
 <div style="display:$disp_div">
-	<hr>
-	<a href="../$day_m1/$additional" style="visibility:$prev_visivility">←前の日の記事</a>　　
-	<a href="../$day_p1/$additional" style="visibility:$post_visivility">→次の日の記事</a>
+  <hr>
+  <a href="../$day_m1/$additional" style="visibility:$prev_visivility">←前の日の記事</a>　　
+  <a href="../$day_p1/$additional" style="visibility:$post_visivility">→次の日の記事</a>
 </div>
 EOD;
-	}
+  }
 
-	if($daycheck || $isPreview){
-		$main_html =  file_get_contents($fileurl);
+  $ogp_flag = false;
+  $ogp_data = "";
+  if($daycheck || $isPreview){
+    $main_html =  file_get_contents($fileurl);
 
-		while(preg_match("/###(\w*.(kn|kn.txt|kn.png))###/", $main_html, $match)){
-			$expand_filename = $match[1];
-			$ext = $match[2];
+    while(preg_match("/###(\w*.(kn|kn.txt|kn.png))###/", $main_html, $match)){
+      $expand_filename = $match[1];
+      $ext = $match[2];
 
-			if($ext == 'kn'){
-				$expand_url = "$year/$dir/src/$expand_filename";
-				if(file_exists($expand_url)){
-					$kn_src =  htmlspecialchars(file_get_contents($expand_url));
+      if($ext == 'kn'){
+        $expand_url = "$year/$dir/src/$expand_filename";
+        if(file_exists($expand_url)){
+          $kn_src =  htmlspecialchars(file_get_contents($expand_url));
 
-					$kn = <<< EOD
+          $kn = <<< EOD
 <div>
-<span style="background-color:#f0f8ff; padding:0 10px 0 10px"><a href="../src/$expand_filename">$expand_filename</a></span>
-<pre class="prettyprint lang-kuin linenums" style="border-radius:0; margin: 0px -10px 0px -10px;">$kn_src</pre>
+  <span style="background-color:#f0f8ff; padding:0 10px 0 10px"><a href="../src/$expand_filename">$expand_filename</a></span>
+  <pre class="prettyprint lang-kuin linenums" style="border-radius:0; margin: 0px -10px 0px -10px;">$kn_src</pre>
 </div>
 EOD;
 
-					$kn2 = "";
-					for($i = 0; $i < strlen($kn); $i++){
-						if($kn[$i] == '\\'){
-							$kn2 .= "\\\\";
-						}else{
-							$kn2 .= $kn[$i];
-						}
-					}
-					$testtest = "[hogehoge]";
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", "\${1}" . $kn2 . "<hr>\${3}", $main_html);
-					#$main_html = preg_split("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", $main_html).join( "\${1}" . $kn2 . "<hr>test4<hr>\${3}");
-					$is_include_kuin_code = true;
-				}else{
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
-				}
-			}else if($ext == 'kn.png'){
-				$expand_url = "$year/$dir/output_images/$expand_filename";
-				if(file_exists($expand_url)){
-					$png = <<< EOD
+          $kn2 = "";
+          for($i = 0; $i < strlen($kn); $i++){
+            if($kn[$i] == '\\'){
+              $kn2 .= "\\\\";
+            }else{
+              $kn2 .= $kn[$i];
+            }
+          }
+          $testtest = "[hogehoge]";
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", "\${1}" . $kn2 . "<hr>\${3}", $main_html);
+          #$main_html = preg_split("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", $main_html).join( "\${1}" . $kn2 . "<hr>test4<hr>\${3}");
+          $is_include_kuin_code = true;
+        }else{
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
+        }
+      }else if($ext == 'kn.png'){
+        $expand_url = "$year/$dir/output_images/$expand_filename";
+        if(file_exists($expand_url)){
+          $png = <<< EOD
 <div style="text-align:center; background-color:#ffffff">
 <img src="../output_images/$expand_filename" alt="[Image:$expand_filename]" style="box-sizing: border-box; max-width:100%; vertical-align: middle; border:3px solid; border-color:#888888"/>
 </div>
 EOD;
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn.png)###([\d\D]*)$/", "\${1}$png\${3}", $main_html);
-				}else{
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn.png)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
-				}
-			}else if($ext == 'kn.txt'){
-				$expand_url = "$year/$dir/output_txt/$expand_filename";
-				if(file_exists($expand_url)){
-					$txt_src =  htmlspecialchars(file_get_contents($expand_url));
-					$txt = <<< EOD
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn.png)###([\d\D]*)$/", "\${1}$png\${3}", $main_html);
+          if(!$ogp_flag){
+            $ogp_flag = true;
+            $ogp_data = <<<EOD
+<meta property="og:title" content="$title" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="$url" />
+<meta property="og:image" content="$base_name/$expand_url" />
+<meta property="og:site_name" content="Tatt.ch" />
+<meta property="og:description" content="$title" />
+<meta name="twitter:card" content="summary" />
+EOD;
+          }
+        }else{
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn.png)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
+        }
+      }else if($ext == 'kn.txt'){
+        $expand_url = "$year/$dir/output_txt/$expand_filename";
+        if(file_exists($expand_url)){
+          $txt_src =  htmlspecialchars(file_get_contents($expand_url));
+          $txt = <<< EOD
 <span style="background-color:#fff8f0; padding:0 10px 0 10px"><a href="../output_txt/$expand_filename">出力結果</a></span>
 <pre style="background-color:#fff8f0; margin: 0px -10px 0px -10px; padding:0px 20px 0px 20px; overflow:auto;">$txt_src</pre>
 EOD;
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn.txt)###([\d\D]*)$/", "\${1}$txt\${3}", $main_html);
-				}else{
-					$main_html = preg_replace("/^([\d\D]*)###(\w*.kn.txt)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
-				}
-			}
-		}
-	}else{
-		$main_html = "※未公開です。当日の午前0時0分に公開されます。";
-	}
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn.txt)###([\d\D]*)$/", "\${1}$txt\${3}", $main_html);
+        }else{
+          $main_html = preg_replace("/^([\d\D]*)###(\w*.kn.txt)###([\d\D]*)$/", "\${1}##$expand_filename##\${3}", $main_html);
+        }
+      }
+    }
+  }else{
+    $main_html = "※未公開です。当日の午前0時0分に公開されます。";
+  }
 
-	print <<< EOD
+  print <<< EOD
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+EOD;
+  if ($ogp_flag) {
+    print $ogp_data;
+  }
+  print <<< EOD
 
-<title>$theme Advent Calendar ${year} - ${day}日目</title>
+<title>$title</title>
 <link rel="stylesheet" href="http://tatt.ch/style.css">
 <style>
-	hr {border-top: 2px solid #B0D4E5;}
+    hr {border-top: 2px solid #B0D4E5;}
 </style>
 
 EOD;
 
-if($is_include_kuin_code){
-	print <<< EOD
+  if($is_include_kuin_code){
+    print <<< EOD
 <link rel="stylesheet" href="http://tatt.ch/kuin/prettify/prettify.css" type="text/css">
-<script type="text/javascript" src="http://tatt.ch/kuin/prettify/prettify.js"></script>
-<script type="text/javascript" src="http://tatt.ch/kuin/prettify/lang-kuin.js"></script>
-<script type="text/javascript">
-	// <![CDATA[
-	(function(){
-		function init(event){
-			prettyPrint();
-		}
-		if(window.addEventListener)window.addEventListener("load",init,false);
-		else if(window.attachEvent)window.attachEvent("onload",init);
-	})();
-	// ]]>
+<script src="http://tatt.ch/kuin/prettify/prettify.js"></script>
+<script src="http://tatt.ch/kuin/prettify/lang-kuin.js"></script>
+<script>
+    // <![CDATA[
+    (function(){
+        function init(event){
+            prettyPrint();
+        }
+        if(window.addEventListener)window.addEventListener("load",init,false);
+        else if(window.attachEvent)window.attachEvent("onload",init);
+    })();
+    // ]]>
 </script>
 
 EOD;
-}
-print <<< EOD
+  }
+  print <<< EOD
 </head>
 <body>
 <div id="container">
-<h1>$theme Advent Calendar ${year} - ${day}日目</h1>
+<h1>$title</h1>
 EOD;
-if($adventar_id != -1){
-print <<< EOD
+  if($adventar_id != -1){
+    print <<< EOD
 <p>この記事は【<a href="http://www.adventar.org/calendars/$adventar_id">$theme Advent Calendar ${year}</a>】の${day}日目の記事です。</p>
 EOD;
-}else{
-print <<< EOD
+  }else{
+    print <<< EOD
 <p>この記事は【$theme Advent Calendar ${year}】の${day}日目の記事です。</p>
 EOD;
-}
-	print $header;
-if($theme == "Kuin"){
-	print <<< EOD
+  }
+  print $header;
+  if($theme == "Kuin"){
+    print <<< EOD
 <p style="color:#008888; font-size:0.8em">【記事中で紹介しているコードについて】<br>
 コンパイルが通らない場合など、不具合があれば、<a href="https://twitter.com/tatt61880">@tatt61880</a>まで連絡いただけると助かります。よろしくお願いいたします。</p><hr>
 EOD;
 
-}
-	print $main_html;
-	print $footer;
+  }
+  print $main_html;
+  print $footer;
 
-	print <<< EOD
+  print <<< EOD
 </div>
 </body>
 </html>
 EOD;
-}
+    }
 ?>
